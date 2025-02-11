90824abb57d5e1e54a9f0423ade6823f3cf65e64
BalavigneshRajasekar <vigneshvickybsc1999@gmail.com>
2025-02-11 23:02:37 +0530

    Essential libraery downloaded

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,13 +1,24 @@
 
 
+import { Button } from 'flowbite-react'
 import './App.css'
 
 function App() {
+
+ 
+
+
+
+ 
  
 
   return (
     <>
-      <h1>Interview Task</h1>
+      <h1 className='bg-red-200 '>Interview Task</h1>
+      <Button color='yellow'>hello</Button>
+
+       
+
        
     </>
   )
--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,7 +1,13 @@
+/* eslint-disable no-unused-vars */
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+import  {Flowbite}  from 'flowbite-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react() ,
+    tailwindcss(),
+    Flowbite(),
+  ],
 })
